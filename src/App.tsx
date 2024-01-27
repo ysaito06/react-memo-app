1bc7d80385ebba9988991832d5d66afa41a3cc98
ysaito06 <y.saito@kuga.tech>
2024-01-27 21:26:22 +0900

    apply react

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const App: React.FC = () => {
   return (
     <div>
-      <h1>Hello, React!</h1>
+      <h1>Hello, React!!</h1>
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
+// main.tsx
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+// clientからimportする
 import App from './App';
 
-// Appコンポーネントをインポート
-
-ReactDOM.createRoot(document.getElementById('app')).render(
+const root = ReactDOM.createRoot(document.getElementById('app') as Element);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
